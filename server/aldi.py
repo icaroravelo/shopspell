c452b1c095423677a890b89f0d4bc6686aec0c2f
Icaro Santos <icaromras@gmail.com>
2024-01-07 03:43:11 +0000

    commit: add auto save into xlsx file

--- server/aldi.py
+++ server/aldi.py
@@ -1,25 +1,69 @@
-import re 
-from playwright.sync_api import sync_playwright, expect
-import time 
+from playwright.sync_api import sync_playwright
+import json
+import time
 
 with sync_playwright() as p:
-    # Set up the browser
+    # Set up browser
     browser = p.chromium.launch(headless=False)
 
     # Create a new page
     page = browser.new_page()
 
-    # Go to the Pingo Doce's website
+    # Go to Continente's website
     page.goto("https://www.aldi.pt/")
 
-    # Locate, click and fill the search text input
-    page.locator('xpath=//*[@id="autocomplete-1-input"]').click()
-    page.fill('xpath=//*[@id="autocomplete-1-input"]', "arroz")
+    # Accept cookies (if necessary)
+    try:
+        page.locator(
+            'xpath=//*[@id="focus-lock-id"]/div/div/div[2]/div/div/div[2]/div/button[3]'
+        ).click()
+    except:
+        pass
 
-    # Click in the search button
-    page.locator('xpath=//*[@id="autocomplete-1-input"]').press("Enter")
+    # Fill the search text input and press Enter
+    search_input = page.locator(
+        '//*[@id="autocomplete-1-input"]'
+    )
+    search_input.click()
+    search_input.fill("arroz")
+    search_input.press("Enter")
 
-    time.sleep(5)
-    
-    # Close browser
-    browser.close()
+    # Wait for the product links to load
+    page.locator(
+        '.tiles-grid'
+    )
+
+    # Get product links
+    product_links = page.evaluate('''() => {
+        const cards = Array.from(document.querySelectorAll('.tiles-grid a'));
+        return cards.map(card => card.getAttribute('href'));
+    }''')
+
+    # Create a variable to store the search results
+    products_data = []
+
+    # Iterate through each product link
+    for link in product_links:
+        page.goto(link)
+
+        # Wait for the product details to load
+        page.locator('.tiles-grid')
+        price = page.locator('.price__label')
+
+        # Get the name and price of the product
+        product_info = page.evaluate('''() => {
+            const title = document.querySelector('.mod-article-intro__header-headline').innerText.trim();
+            const price = document.querySelector('price__label').innerText.trim();
+
+            return { title, price };
+        }''')
+
+        # Append the product info to the list
+        products_data.append(product_info)
+
+    # Output the collected data
+    print(products_data)
+
+    # Saving information in a JSON file
+    with open('product_info.json', 'w') as json_file:
+        json.dump(products_data, json_file)
